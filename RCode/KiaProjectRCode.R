a3e9203c6a8bedc2cd017cc6486e2641dabb9d72
jacksont83 <thomas.jackson@healthgrades.com>
2020-06-26 11:09:54 -0700

    Changing names

--- RCode/KiaProjectRCode.R
+++ RCode/KiaProjectRCode.R
@@ -1,7 +1,8 @@
 rm(list = ls())
 
 # Reading in data ---------------------------------------------------------
-kia.data <- read.csv("./Data/MarineJulyTrainingAnalysis.csv")
+kiah.data <- read.csv("./Data/MarineJulyTrainingAnalysis.csv")
+names(kiah.data)[8:dim(kiah.data)[2]] <- as.character(rev(seq(as.Date('2020-05-25'),as.Date('2020-06-24'),1)))
 
 # Examine by State --------------------------------------------------------
 
