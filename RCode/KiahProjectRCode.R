fd36e01ffe1356e95c42a507f0f47c75c730577c
jacksont83 <thomas.jackson@healthgrades.com>
2020-07-13 16:14:56 -0700

    Fixed code.

--- RCode/KiahProjectRCode.R
+++ RCode/KiahProjectRCode.R
@@ -2,8 +2,8 @@ rm(list = ls())
 
 # Reading in data ---------------------------------------------------------
 kiah.data <- read.csv("./Data/MarineJulyTrainingAnalysis.csv")
-names(kiah.data)[8:dim(kiah.data)[2]] <- as.character(rev(seq(as.Date('2020-05-25'),as.Date('2020-06-24'),1)))
-kiah.data <- kiah.data[,na.omit(match(c(names(kiah.data)[1:7], as.character(seq(as.Date('2020-05-25'),as.Date('2020-06-24'),1))), names(kiah.data)))]
+names(kiah.data)[8:dim(kiah.data)[2]] <- as.character(rev(seq(as.Date('2020-05-29'),as.Date('2020-07-12'),1)))
+kiah.data <- kiah.data[,na.omit(match(c(names(kiah.data)[1:7], as.character(seq(as.Date('2020-05-29'),as.Date('2020-07-12'),1))), names(kiah.data)))]
 
 # Examine by State --------------------------------------------------------
 unq.states <- unique(kiah.data$State[order(kiah.data$State)])
@@ -35,20 +35,20 @@ plot(
   ylab = "Confirmed Infections",
   main = "Corona By State"
 )
-rect(18400, -20000,18440, 160000, col = "grey90")
-x.lines <- seq(as.Date("2020-05-25"), as.Date("2020-06-22"), 7)
+rect(18400, -20000,184400, 1600000, col = "grey90")
+x.lines <- seq(as.Date("2020-05-29"), as.Date("2020-07-12"), 7)
 for(i in 1:length(x.lines)){abline(v = x.lines[i], col = "white")}
-y.lines <- seq(-20000, 160000, 20000)
+y.lines <- seq(-20000, 1600000, 20000)
 for(i in 1:length(y.lines)){abline(h = y.lines[i], col = "white")}
-axis(1, seq(18400, 18440, 40), rep(NA, 2))
-axis(2, seq(-20000, 160000, 20000))
+axis(1, seq(18400, 184400, 40), rep(NA, 2))
+axis(2, seq(-20000, 1600000, 20000))
 for(i in 1:length(unq.states)){
   points(
     state.data$virus.count[state.data$state == unq.states[i]] ~ state.data$day[state.data$state == unq.states[i]],
     col = i,
     type = 'l'
   )
-  print(paste0(100*round(i/length(unq.states), 4), "% - Combining States"))
+  print(paste0(100*round(i/length(unq.states), 4), "% - Plotting States"))
 }
 
 
@@ -64,7 +64,7 @@ plot(
   main = "Corona By State",
   ylim = c(0, 40000)
 )
-rect(18400, -20000,18440, 160000, col = "grey90")
+rect(18400, -20000,184400, 160000, col = "grey90")
 x.lines <- seq(as.Date("2020-05-25"), as.Date("2020-06-22"), 7)
 for(i in 1:length(x.lines)){abline(v = x.lines[i], col = "white")}
 y.lines <- seq(-20000, 160000, 20000)
